95ffb7f52340776971135ff729c2f1e6f5307d6e
meron <meron@example.com>
2022-12-19 21:35:33 +0000

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,8 +4,8 @@
 
 exec { 'command':
   command  => 'apt-get -y update;
+  provider => shell,
   apt-get -y install nginx;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
-  provider => shell,
 }
