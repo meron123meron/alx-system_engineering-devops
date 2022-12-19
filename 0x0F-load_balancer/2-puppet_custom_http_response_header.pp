4d45af0bdb3c0ffb5ecc859e2a25fea2a049584b
meron <meron@example.com>
2022-12-19 21:38:51 +0000

    3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,8 +3,8 @@
 #The value of the custom HTTP header must be the hostname of the server Nginx is running on
 
 exec { 'command':
-  command  => 'apt-get -y update;
   provider => shell,
+  command  => 'apt-get -y update;
   apt-get -y install nginx;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
