032b2e79cb8a450e5a3967655f087de0f8ed35d7
meron <meron@example.com>
2023-02-01 19:58:32 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers for a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the total number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
